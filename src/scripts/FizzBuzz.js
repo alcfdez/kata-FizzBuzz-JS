5e3ebe4c52fb667c97727652e97da458c58367d0
alcfdez <alcfdez@hotmail.com>
2023-02-06 09:28:11 +0100

    corregido error ultimo test

--- src/scripts/FizzBuzz.js
+++ src/scripts/FizzBuzz.js
@@ -1,18 +1,19 @@
- export function fizzBuzz(number){
-     if (number % 3 == 0){
-         return "Fizz";
-
-     }
-
-     if(number % 5 == 0){
-         return "Buzz";
-     }
-
-
-
-    if(number % 3 == 0 && number % 5 == 0){
+export function fizzBuzz(number) {
+    if (number % 5 == 0 && number % 3 == 0) {
         return "FizzBuzz";
     }
+
+    if (number % 3 == 0) {
+        return "Fizz";
+
+    }
+
+    if (number % 5 == 0) {
+        return "Buzz";
+    }
+
+
+
 }
 
 
